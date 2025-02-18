af50eb0d2345f9ca76f19c2ff7fd473f2924d0f6
Danilo Jimenez <danilojimenez@exsis.com.co>
2025-02-18 11:49:38 -0500

    Add migration - Third Stage

--- src/Infrastructure/Persistance/Configuration/EmployeeConfiguration.cs
+++ src/Infrastructure/Persistance/Configuration/EmployeeConfiguration.cs
@@ -33,7 +33,8 @@ public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
 
         builder.Property(c => c.BloodType).HasMaxLength(3);
 
-        builder.Property(c => c.Salary);
+        builder.Property(c => c.Salary)
+            .HasColumnType("decimal(18,4)");
 
         builder.Property(c => c.Phone)
            .HasConversion(
